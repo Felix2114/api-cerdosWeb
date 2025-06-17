2838a44e5782870444d6a9317ab34209cf664671
Felix2114 <zS23004696@estudiantes.uv.mx>
2025-06-17 15:47:44 -0600

    update2

--- src/main/java/com/felix/cerdos/security/postgresql/cerdos/controllers/CommentController.java
+++ src/main/java/com/felix/cerdos/security/postgresql/cerdos/controllers/CommentController.java
@@ -26,7 +26,7 @@ import com.felix.cerdos.security.postgresql.cerdos.repository.TweetRepository;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
-
+@CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
 @RequestMapping("/api/comments")
 
